e4db00cbea06ae84ec5c49f11333f0d5a2df7a95
IrvanSN <30742772+IrvanSN@users.noreply.github.com>
2022-05-07 12:28:07 +0700

    update team page

--- app/team/controller.js
+++ app/team/controller.js
@@ -2,14 +2,43 @@ const Team = require('./model');
 
 module.exports = {
   index: async (req, res) => {
-    res.send('ok');
+    const { status } = req.query;
+
+    if (status === 'active') {
+      Team.find({ status: true })
+        .then((r) => {
+          const count = r.length;
+
+          return res.render('team/index.ejs', {
+            title: 'Tim Aktif - PLAYBOX Season 3', team: r, status, count,
+          });
+        });
+    } else if (status === 'inactive') {
+      Team.find({ status: false })
+        .then((r) => {
+          const count = r.length;
+
+          return res.render('team/index.ejs', {
+            title: 'Tim Aktif - PLAYBOX Season 3', team: r, status, count,
+          });
+        });
+    } else {
+      Team.find({})
+        .then((r) => {
+          const count = r.length;
+
+          return res.render('team/index.ejs', {
+            title: 'Tim Aktif - PLAYBOX Season 3', team: r, status, count,
+          });
+        });
+    }
   },
   detailTeam: async (req, res) => {
     const { id } = req.params;
 
     await Team.findById(id)
       .then((r) => {
-        res.render('team/index.ejs', { title: `${r.name} - PLAYBOX Season 3`, team: r });
+        res.render('team/view-detail.ejs', { title: `${r.name} - PLAYBOX Season 3`, team: r });
       }).catch(() => {
         res.redirect('/');
       });
@@ -20,9 +49,23 @@ module.exports = {
 
     await Team.findByIdAndUpdate(id, { status })
       .then((r) => {
-        res.redirect(`/team/${r._id}`);
+        res.redirect('/team');
       }).catch(() => {
         res.redirect('/');
+      });
+  },
+  searchTeam: async (req, res) => {
+    const { value } = req.body;
+    const regex = new RegExp(value);
+
+    Team.find({ name: { $regex: regex, $options: 'i' } })
+      .then((r) => {
+        console.log(r);
+        const count = r.length;
+
+        return res.render('team/index.ejs', {
+          title: 'Tim Aktif - PLAYBOX Season 3', team: r, status: 'search', count,
+        });
       });
   },
 };
--- app/team/routes.js
+++ app/team/routes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const router = express.Router();
-const { index, detailTeam, updateTeam } = require('./controller');
+const {
+  index, detailTeam, updateTeam, searchTeam,
+} = require('./controller');
 const { isLoginAdmin } = require('../../middleware/auth');
 
 router.get('/', isLoginAdmin, index);
 router.get('/:id', isLoginAdmin, detailTeam);
 router.post('/:id/update', isLoginAdmin, updateTeam);
+router.post('/search', isLoginAdmin, searchTeam);
 
 module.exports = router;
